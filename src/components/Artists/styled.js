61bbb7177f92b0f36197ea9bb0d6598d68e5f9db
gabriel-tomas <gabriel.tomas.com@gmail.com>
2024-02-24 16:52:49 -0300

    feature: media queries para ajustar fotos com tamanhos diferentes

--- src/components/Artists/styled.js
+++ src/components/Artists/styled.js
@@ -18,16 +18,134 @@ export const ContainerArtist = styled.div`
   padding: 1rem;
   border-radius: 1rem;
   cursor: pointer;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 
   .container-img {
-    width: 100%;
+    width: 150px;
+    height: 150px;
+    border-radius: 50%;
+    overflow: hidden;
     background-color: transparent;
+    transition: .2s width, .2s height ;
 
-    img {
-      border-radius: 50%;
+
+    @media screen and (min-width: 1200px) {
+      width: 155px;
+      height: 155px;
+    }
+
+    @media screen and (max-width: 1440px) {
+      width: 170px;
+      height: 170px;
+    }
+
+    @media screen and (max-width: 1350px) {
+      width: 155px;
+      height: 155px;
+    }
+
+    @media screen and (max-width: 1043px) {
+      width: 140px;
+      height: 140px;
+    }
+
+    @media screen and (max-width: 976px) {
+      width: 130px;
+      height: 130px;
+    }
+
+    @media screen and (max-width: 921px) {
+      width: 168px;
+      height: 168px;
+    }
+
+    @media screen and (max-width: 880px) {
+      width: 158px;
+      height: 158px;
+    }
+
+    @media screen and (max-width: 832px) {
+      width: 150px;
+      height: 150px;
+    }
+
+    @media screen and (max-width: 832px) {
+      width: 140px;
+      height: 140px;
+    }
+
+    @media screen and (max-width: 772px) {
+      width: 130px;
+      height: 130px;
+    }
+
+    @media screen and (max-width: 736px) {
+      width: 128px;
+      height: 128px;
+    }
+
+    @media screen and (max-width: 732px) {
+      width: 170px;
+      height: 170px;
+    }
+
+    @media screen and (max-width: 660px) {
+      width: 150px;
+      height: 150px;
+    }
+
+    @media screen and (max-width: 600px) {
+      width: 130px;
+      height: 130px;
+    }
+
+    @media screen and (max-width: 520px) {
+      width: 175px;
+      height: 175px;
+    }
+
+    @media screen and (max-width: 455px) {
+      width: 160px;
+      height: 160px;
+    }
+
+    @media screen and (max-width: 433px) {
+      width: 140px;
+      height: 140px;
+    }
+
+    @media screen and (max-width: 379px) {
+      width: 130px;
+      height: 130px;
+    }
+
+    @media screen and (max-width: 341px) {
+      width: 200px;
+      height: 200px;
+    }
+
+    @media screen and (max-width: 256px) {
+      width: 170px;
+      height: 170px;
+    }
+
+    @media screen and (max-width: 231px) {
+      width: 140px;
+      height: 140px;
+    }
+
+    @media screen and (max-width: 195px) {
       width: 100%;
       height: 100%;
-      object-fit: contain;
+      border-radius: 0;
+    }
+
+    img {
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
       box-shadow: 0 5px 5px rgba(0,0,0, 0.17);
     }
   }
@@ -36,6 +154,7 @@ export const ContainerArtist = styled.div`
     display: flex;
     flex-direction: column;
     gap: .2rem;
+    width: 100%;
 
     .artist-name {
       font-size: ${fontSizes.fontSizeBase};
