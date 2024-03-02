20fde2aff8712c7ae28824c31553fcc7b7e1d27a
gabriel-tomas <gabriel.tomas.com@gmail.com>
2024-03-02 18:48:20 -0300

    i will revert

--- src/services/backend/index.js
+++ src/services/backend/index.js
@@ -2,5 +2,4 @@ import axios from 'axios';
 
 export default axios.create({
   baseURL: 'https://music-app-api-5r44.onrender.com',
-  withCredentials: true,
 });
--- src/store/modules/checkLogin/sagas.js
+++ src/store/modules/checkLogin/sagas.js
@@ -1,4 +1,5 @@
 import { call, put, all, takeLatest } from 'redux-saga/effects';
+import axios from 'axios';
 /* import { toast } from 'react-toastify'; */
 
 import backendApi from '../../../services/backend';
@@ -9,9 +10,32 @@ import * as types from '../types';
 
 function* checkRequest() {
   try {
-    const response = yield call(backendApi.get, '/checkLogin');
+    /* const response = yield call(backendApi.get, '/checkLogin', {
+      withCredentials: true,
+    }); */
+
+    // Agora você pode fazer suas solicitações usando o Axios
+    const options = {
+      method: 'GET',
+      url: 'https://music-app-api-5r44.onrender.com/checkLogin',
+      withCredentials: true,
+      headers: {
+        'Content-Type': 'application/json',
+        httpOnly: true,
+      },
+    };
+
+    axios
+      .request(options)
+      .then(function (response) {
+        console.log(response.data);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+    /* console.log(response);
     const isLoggedIn = response.data.login;
-    console.log(response.data);
+    console.log(response.data); */
     if (!isLoggedIn) {
       yield put(checkLoginActions.checkLoginFail());
     } else {
