34ca3026ee4e56c9551eb5e02603f5418f9ed51e
gabriel-tomas <gabriel.tomas.com@gmail.com>
2024-03-04 19:29:03 -0300

    feature: logout functionality

--- src/pages/Account/LoggedContent/index.jsx
+++ src/pages/Account/LoggedContent/index.jsx
@@ -1,9 +1,22 @@
+import { useDispatch } from 'react-redux';
 import { ContainerLoggedContent } from './styled';
+import { toast } from 'react-toastify';
+
+import * as authActions from '../../../store/modules/auth/actions';
 
 export default function NotLogged() {
+  const dispatch = useDispatch();
+
+  const handleLogout = () => {
+    dispatch(authActions.authFail());
+    toast.info('Logout feito com sucesso');
+  };
+
   return (
     <ContainerLoggedContent>
-      <button className="logout-button">Sair da conta</button>
+      <button className="logout-button" onClick={handleLogout}>
+        Sair da conta
+      </button>
     </ContainerLoggedContent>
   );
 }
