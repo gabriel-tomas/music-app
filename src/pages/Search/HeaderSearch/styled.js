f0e2487fed096200a88fbd1685052102d97fcc2b
gabriel-tomas <gabriel.tomas.com@gmail.com>
2024-03-08 23:23:36 -0300

    fix: search bar in search page
    100% search bar width

--- src/pages/Search/HeaderSearch/styled.js
+++ src/pages/Search/HeaderSearch/styled.js
@@ -8,8 +8,21 @@ export default styled.header`
   grid-template-columns: 1fr 1fr 1fr;
   margin-bottom: 1.4rem;
   margin-top: -1.5rem;
+  transition: .15s grid-template-columns;
+
+  @media screen and (max-width: 975px) {
+    grid-template-columns: 1fr 1.5fr 1fr;
+  }
+
+  @media screen and (max-width: 780px) {
+    grid-template-columns: 1fr 2fr 1fr;
+  }
 
   @media screen and (max-width: 600px) {
     grid-template-columns: 1fr 1.8fr 1fr;
+  }
+
+  @media screen and (max-width: 500px) {
+    grid-template-columns: 1fr 32fr 1fr;
   }
 `;
