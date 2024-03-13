d17c310f945ed891a2e42a7d9a68e8beb5a644d3
gabriel-tomas <gabriel.tomas.com@gmail.com>
2024-03-12 23:17:24 -0300

    feature: options for account page

--- src/pages/Account/LoggedContent/index.jsx
+++ src/pages/Account/LoggedContent/index.jsx
@@ -1,8 +1,13 @@
 import { useDispatch } from 'react-redux';
-import { ContainerLoggedContent } from './styled';
+import { Link } from 'react-router-dom';
 import { toast } from 'react-toastify';
+import { LuUser2 } from 'react-icons/lu';
+import { IoInformationOutline } from 'react-icons/io5';
+import { MdKeyboardArrowRight } from 'react-icons/md';
 
 import * as authActions from '../../../store/modules/auth/actions';
+
+import { ContainerOptions, ContainerLoggedContent } from './styled';
 
 export default function NotLogged() {
   const dispatch = useDispatch();
@@ -14,6 +19,22 @@ export default function NotLogged() {
 
   return (
     <ContainerLoggedContent>
+      <ContainerOptions>
+        <Link className="option" to="/account/data">
+          <div className="left-container">
+            <LuUser2 />
+            <span>Meus dados</span>
+          </div>
+          <MdKeyboardArrowRight className="arrow-right" />
+        </Link>
+        <button className="option">
+          <div className="left-container">
+            <IoInformationOutline />
+            <span>Informação</span>
+          </div>
+          <MdKeyboardArrowRight className="arrow-right" />
+        </button>
+      </ContainerOptions>
       <button className="logout-button" onClick={handleLogout}>
         Sair da conta
       </button>
--- src/pages/Account/LoggedContent/styled.js
+++ src/pages/Account/LoggedContent/styled.js
@@ -7,6 +7,9 @@ export const ContainerLoggedContent = styled.div`
   display: flex;
   justify-content: left;
   flex-direction: column;
+  justify-content: space-between;
+  min-height: calc(100vh - 10rem);
+  min-height: calc(100svh - 10rem);
 
   button {
     font-size: ${fontSizes.fontSizeBase};
@@ -24,5 +27,61 @@ export const ContainerLoggedContent = styled.div`
   button.logout-button {
     background-color: ${colors.primary['300']};
     border-radius: 20rem;
+  }
+
+  @media screen and (max-width: 600px) {
+    min-height: calc(100vh - 13rem);
+    min-height: calc(100svh - 13rem);
+  }
+`;
+
+export const ContainerOptions = styled.div`
+  .option {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    font-size: ${fontSizes.fontSizeBase};
+
+    .left-container {
+      display: flex;
+      align-items: center;
+      gap: 1rem;
+
+      svg {
+        height: 100%;
+        width: 24px;
+      }
+    }
+
+    & > svg {
+      height: 100%;
+      width: 24px;
+    }
+  }
+
+  @media (hover: hover) {
+    .option:hover {
+      background-color: ${colors.primary['200']};
+    }
+  }
+
+  button.option:active, a.option:active {
+    background-color: ${colors.primary['300']};
+  }
+
+  .option + .option {
+    margin-top: clamp(0.7rem, 0.485rem + 0.916vw, 1rem);
+  }
+
+  button.option, .option {
+    height: 64px;
+    padding: .8rem;
+    border-radius: .6rem;
+  }
+
+  button.option {
+    width: 100%;
+    margin: unset;
+    font-weight: normal;
   }
 `;
