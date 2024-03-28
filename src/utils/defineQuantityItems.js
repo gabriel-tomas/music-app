44c58a521fee631c90e22c5940791b1bf714c5ed
gabriel-tomas <gabriel.tomas.com@gmail.com>
2024-03-28 19:29:15 -0300

    fix: how much items show based on users browser width

--- src/utils/defineQuantityItems.js
+++ src/utils/defineQuantityItems.js
@@ -1,7 +1,14 @@
 export default () => {
-  if (window.innerWidth >= 1770) {
+  if (window.innerWidth >= 2048) {
+    return 16;
+  } else if (window.innerWidth >= 1768) {
+    return 14;
+  } else if (window.innerWidth >= 1534) {
     return 12;
-  } else if (window.innerWidth >= 1250) {
+  } else if (window.innerWidth >= 1270) {
+    return 10;
+  } else if (window.innerWidth >= 906) {
     return 8;
   }
+  return 6;
 };
